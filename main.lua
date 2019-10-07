8a877e164c9300ca479550aa6e00d2676d6e4be3
Michal Pekacki <44163300+MichalPekacki@users.noreply.github.com>
2019-10-07 18:32:29 +0100

    Add files via upload

--- main.lua
+++ main.lua
@@ -10,7 +10,7 @@ local timeElapsed = os.clock()
 local eventTime = os.clock()
 
 local followers = 0 --current followers
-local fpc = 1 --how many followers are added every tap
+local fpc = 1 --how many followers are added every tap (followers per click)
 local fpcUp = 1 --addded to fpc when entered next stage
 local levelUp = 50 --amount at which user enters next stage
 local cprMultiplier = 1 --a number fpc is multiplied by
@@ -23,7 +23,7 @@ local timeEventOn = false --determines if timed event is on
 
 local stage = 1 --stage count
 local nrToEvent = 1 --counts when timed events should occur
-local nrToChoiceEvent = 1 --count when choice events should occur
+local nrToChoiceEvent = 1 --counts when choice events should occur
 
 --variables storing random number called in functions
 local randomNumber
@@ -31,7 +31,9 @@ local eventRandomNumber
 
 --IMAGES--
 
-local background = display.newImageRect( "background.jpg", display.actualContentWidth, display.actualContentHeight )
+local backgroundImages = {"background.jpg", "stage2.jpg"}
+
+local background = display.newImageRect( backgroundImages[1], display.actualContentWidth, display.actualContentHeight )
 background.x = display.contentCenterX
 background.y = display.contentCenterY
 
@@ -52,7 +54,7 @@ local fpcText = display.newText( fpc, 280, 100, native.systemFont, 20 )
 
 local newsText = display.newText( "", display.contentCenterX, 230, native.systemFont, 20 )
 newsText.anchorX = 0 --set text anchor point to the far left side of text
-local randomNews = {"hello to the world of follower frenzy", "there is a garden hose in my shoe", "ronald thump becomes president", "canyon south gives out weevies"} --array containing all news text
+local randomNews = {"hello to the world of Follower Frenzy", "story nr 2", "story nr 3", "story nr 4"} --array containing all news text
 
 local eventText = display.newText( "", display.contentCenterX, 280, native.systemFont, 22 )
 local acceptButton = display.newText( "", 80, 320, native.systemFont, 21 )
@@ -60,11 +62,12 @@ local declineButton = display.newText( "", display.actualContentWidth - 80, 320,
 local eventAnswer = display.newText( "", display.contentCenterX, 300, native.systemFont, 22 )
 local option1 = {"Invited to a tv show", "Featured in a magazine", "Mentioned by a celebrity"} --array containing all event option 1 text
 local option2 = {"You were the life\nof the party", "You tried to act\ncool but it backfired",
- "You looked amazing\nand people are interested", "You were caught on\nyour bad day",
+ "You looked amazing\nand people are taking interest", "You were caught on\nyour bad day",
  "They appriciated your\nwork", "They think you will\nnever make it"} --array containing all event option 2 text
 
 --EVENT OPTIONS--
 local function eventOption1()
+	eventAnswer.text = ""
 	eventRandomNumber = math.random (1, 3)
 	eventText.text = option1[eventRandomNumber]
 	acceptButton.text = "accept"
@@ -123,13 +126,14 @@ end
 --FOLLOWERS PER CLICK--
 
 local function incrFollowers()
-	--next stage upgrades and other multiplications
+	--next stage upgrades and multiplications
 	if followers >= levelUp then
 		fpc = fpc + fpcUp --when the timed event is on this multiplier will assure the fpc stays the same when levelUp occurs
 		levelUp = levelUp * 2
 		stage = stage + 1
 		nrToEvent = nrToEvent + 1
 		nrToChoiceEvent = nrToChoiceEvent + 1
+		background = display.newImageRect( backgroundImages[stage], display.actualContentWidth, display.actualContentHeight ) --change graphics
 	end
 	
 --TIMED EVENT--
@@ -186,8 +190,6 @@ local function init()
 			timeElapsed = timeElapsed + 2 --every 2 seconds ^^^ add the 2 seconds gap which essentially resets the clock
 		end
 	end
-	followersText.text = math.floor(followers) --constantly updating the followers amount text, also round down in case of a decimal
-    --print(string.format("elapsed time: %.2f\n", os.clock() - eventTime)) --display counting time at console for testing purposes
 
 --TIMED EVENT CLOCK--
 	if timeEventOn == true then
@@ -200,11 +202,15 @@ local function init()
 	
 --NEWS MOVEMENT AND UPDATE--
 	newsText.x = newsText.x - 0.5 --move text to the left. speed depends on frames but does not affect gameplay
-	if ( newsText.x + newsText.width ) <= 0 then --left side + the width of text = right side
+	if ( newsText.x + newsText.width ) <= 0 then --left side + the width of text = right side x point is less than 0
 		randomNumber = math.random (1, 4) --pick random news text from the array specified earlier
 		newsText.text = randomNews[randomNumber] --update text
 		newsText.x = display.actualContentWidth --reset text position on the right side of screen
 	end
+	
+	followersText.text = math.floor(followers) --constantly updating the followers amount text, also round down in case of a decimal
+	--print(string.format("elapsed time: %.2f\n", os.clock() - eventTime)) --display counting time at console for testing purposes
+	
 end
 
 --FUNCTION CALLS--
@@ -224,15 +230,18 @@ Runtime:addEventListener("enterFrame", init)
 
 --tap to get followers ( all calculations included )
 --upgrade computer
---managers(4 managers)( ( 5mil * 8 etc ) first at 5mil then 40mil then 320mil then 2,560mil ) ( 10,000 every 5/10 sec then * 10 so 100,000 then 1mil then 10 mil )
+--managers(max 4 managers)( ( 5mil * 8 etc ) first at 5mil then 40mil then 320mil then 2,560mil ) ( 10,000 every 5/10 sec then * 10 so 100,000 then 1mil then 10 mil )
 --timed event ( new timer, times fpc and fpcUP by 2, after timer is done divide fpc and fpcUP by 2) fpcUP is the amount that is added per click )
 --news feed ( appear constantly, world related )
 --choice event ( accept / decline )
 
 --IN PROGRESS--
 
+--upgrade locations and a system to change the graphics ( done at lvl up ) ( max 30 stages before max population )
+
 --INCOMPLETE--
 
---upgrade locations and a system to change the graphics ( done at lvl up )
+--automatically save game progress (stage and followers, when loading load the graphic corresponding to the stage)
+--home screen, instructions screen
 --graphics
---customize
+--overall look and feedback of the game to the user ( testing )
